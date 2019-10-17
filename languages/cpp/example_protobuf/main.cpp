5167ddc4303bb5c526d3114ad2cc40686291c89e
Aaron <aaronchongth@gmail.com>
2019-10-17 17:03:19 +0800

    more information in protobuf example

--- languages/cpp/example_protobuf/main.cpp
+++ languages/cpp/example_protobuf/main.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <iostream>
 #include <memory>
 #include <proto/example_msg.pb.h>
 
@@ -24,6 +25,38 @@ int main(int argc, char** argv)
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
   example_package::AddressBook address_book;
+  
+  // lets add a repeated field element
+  // only when adding repeated fields will raw pointers need to be handled
+  // another way is to extend the generated header and library
+  example_package::Person* new_person_1 = address_book.add_people();
+  new_person_1->set_name("david_1");
+  new_person_1->set_id(1);
+
+  // required fields in proto2 syntax allow us to check if the field has been
+  // set
+  if (new_person_1->has_high_school())
+    std::cout << "without allocating, it said has, booo" << std::endl;
+  else
+    std::cout << "without allocating, it is considered missing, yay" << std::endl;
+
+  // lets add another repeated field element, this time with all fields
+  example_package::Person* new_person_2 = address_book.add_people();
+  new_person_2->set_name("david_2");
+  new_person_2->set_id(2);
+
+  // using ownership transfer to avoid raw pointers
+  auto school_msg = std::make_unique<example_package::School>();
+  school_msg->set_name("Carnegie Mellon");
+  school_msg->set_graduate_year(2018);
+  new_person_2->set_allocated_high_school(school_msg.release());
+
+  if (new_person_2->has_high_school())
+    std::cout << "allocated, considered, nice!" << std::endl;
+  else
+    std::cout << "allocated, bug considered missing, =.=" << std::endl;
+  
+  // repeated fields can be added indefinitely
   for (int i = 0; i < 10; ++i)
   {
     // only when adding repeated fields will raw pointers need to be handled
@@ -32,7 +65,6 @@ int main(int argc, char** argv)
     new_person->set_name("david");
     new_person->set_id(i);
 
-    // using ownership transfer to avoid raw pointers
     auto school_msg = std::make_unique<example_package::School>();
     school_msg->set_name("John Doe High");
     school_msg->set_graduate_year(2019);
