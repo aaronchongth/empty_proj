b4959febb6712276f6a97861b8a1096a4419a1c7
Aaron Chong <aaronchongth@gmail.com>
2021-03-17 11:27:17 +0800

    Removed vscodium and added ccache
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- configs/new_setup.sh
+++ configs/new_setup.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update && sudo apt upgrade -y
 
 sudo apt-get install \
-  gcc cmake make wget \
+  gcc \
+  cmake \
+  make \
+  wget \
   build-essentials \
-  vim git unzip \
-  snapd openvpn \
-  openssh-client openssh-server \
+  vim \
+  git \
+  unzip \
+  snapd \
+  openvpn \
+  openssh-client \
+  openssh-server \
   python3-vcstool \
   libeigen3-dev \
-  htop ffmpeg \
+  htop \
+  ffmpeg \
   -y
 
-# vscodium
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | sudo apt-key add - 
-
-echo 'deb https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
-
-sudo apt update && sudo apt install codium -y
+# Setup ccache
+sudo apt install ccache -y
+sudo /usr/sbin/update-ccache-symlinks
+echo 'export PATH="/usr/lib/ccache:$PATH"' | tee -a ~/.bashrc
+source ~/.bashrc
 
 # gimp 2.10
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
